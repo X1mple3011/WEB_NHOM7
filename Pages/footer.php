3b4d35511ed01d93792c6ad44c6a74304b8b1bd7
loicr72003 <hoangloi04102003@gmail.com>
2024-08-01 16:00:07 +0700

    update header, footer, dangnhap, dangky

---
+++ Pages/footer.php
@@ -0,0 +1,70 @@
+<footer class="footer border">
+    <div class="container footer-container">
+        <!-- <h1>FOOTER</h1> -->
+        <ul>
+            <h4>Chính Sách</h4>
+            <li><a href="#">Chính Sách Bảo Mật Thông Tin Khách Hàng</a></li>
+            <li><a href="#">Chính Sách Khách Hàng Thân Thiết</a></li>
+            <li><a href="#">Chính Sách Sử Dụng Cookies</a></li>
+            <li><a href="#">Chính Sách Thanh Toán</a></li>
+            <li><a href="#">Chính Sách Bảo Hành</a></li>
+
+
+
+        </ul>
+        <ul>
+            <h4>Về Shoppeng</h4>
+            <li><a href="#">Giới Thiệu Về Shoppeng</a></li>
+            <li><a href="#">Điều Khoản Shoppeng</a></li>
+            <li><a href="#">Tuyển Dụng</a></li>
+            <li><a href="#">Liên Hệ</a></li>
+            <li>
+                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-tiktok"></i></a>
+            </li>
+            <!-- <li><a href="#">Chính Hãng</a></li> -->
+        </ul>
+        <ul>
+            <h4>Trung Tâm Hỗ Trợ</h4>
+            <li>
+                <i class="fa-regular fa-envelope"></i>
+                Email:
+                <a href="#">
+                    shoppenchamsockhachhang@gmail.com
+                </a>
+            </li>
+            <li>
+                <i class="fa-solid fa-phone"></i>
+                Liên Hệ Đặt Hàng:
+                <a href="#">024 999 42 666</a>
+            </li>
+            <li>
+                <i class="fa-solid fa-phone"></i>
+                Góp Ý, Khiếu Nại:
+                <a href="#">042 666 24 999</a>
+            </li>
+
+
+        </ul>
+        <ul>Tải Ứng Dụng Shoppeng
+            <div class="download-app">
+                <li style=" background-color: red;">
+                    <a href="#"><img src="https://down-vn.img.susercontent.com/file/a5e589e8e118e937dc660f224b9a1472" alt="" style="width: 100%;"></a>
+                </li>
+
+
+                <li>
+                    <a href="#"><img src="https://down-vn.img.susercontent.com/file/ad01628e90ddf248076685f73497c163" alt="" style="width: 100%;height: 50%;"></a>
+                    <a href="#"><img src="https://down-vn.img.susercontent.com/file/ae7dced05f7243d0f3171f786e123def" alt="" style="width: 100%;height: 50%;"></a>
+                </li>
+
+            </div>
+        </ul>
+
+
+
+
+    </div>
+</footer>
